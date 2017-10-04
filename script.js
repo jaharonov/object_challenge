a54185536da47b1aa9101c9f5c15d663295d3dbe
Jaffa Aharonov <jaharonov@gmail.com>
2017-10-03 23:32:36 -0500

    not quite there

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 var locations = []; // this array will hold your objects
 
 // object constructor
+var Locations = function(state, revenue, locationCount){
+  this.state = state;
+  this.revenue = revenue;
+  this.locationCount = locationCount;
+  this.averageRevenue = function() {
+  return Math.round(this.revenue / this.locationCount);
+    };
 
+};
 // object instances
-
+var illinois = {state: "IL",revenue: 5000,locationCount: 12};
+var minnesota = {state: "MN",revenue: 300,locationCount: 3};
+var nevada = {state: "NV",revenue: 25000, locationCount: 1};
+var locationStuff = [illinois, minnesota, nevada];
 // push object instances to locations array
+locations.push(locationStuff);
 
 // DO NOT MODIFY THIS CODE
 $(document).ready(function () {
